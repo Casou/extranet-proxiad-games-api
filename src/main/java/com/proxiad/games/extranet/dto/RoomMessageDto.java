504adcf7a43adfa56a705882819bc4c3c5dc08ef
Basile <basile.parent@arvato.fr>
2019-01-28 15:44:59 +0100

    Ajout de la phrase d'intro dans les messages WS

--- src/main/java/com/proxiad/games/extranet/dto/RoomMessageDto.java
+++ src/main/java/com/proxiad/games/extranet/dto/RoomMessageDto.java
@@ -1,5 +1,6 @@
 package com.proxiad.games.extranet.dto;
 
+import com.proxiad.games.extranet.model.IntroSentence;
 import com.proxiad.games.extranet.model.Room;
 
 import lombok.Data;
@@ -9,5 +10,6 @@ public class RoomMessageDto {
 
 	private Room room;
 	private String message;
+	private IntroSentence introSentence;
 
 }
